f9593035b9c804584b3c117c20b7c1d1abb102d4
Delfinzito <146035305+Delfinzito@users.noreply.github.com>
2023-10-10 23:54:28 -0300

    commit init
    sdadsada

---
+++ js/dadsa.js
@@ -0,0 +1,12 @@
+// O JavaScript não é estritamente necessário para fazer o link pulsar, mas pode ser usado para adicionar comportamentos extras quando o link é clicado.
+document.getElementById("pulsating-link").addEventListener("click", function() {
+    // Adicione aqui qualquer ação que você deseja que ocorra quando o link é clicado.
+    alert("Você clicou no link pulsante!");
+  });
+
+ // O JavaScript não é estritamente necessário para fazer o link pulsar, mas pode ser usado para adicionar comportamentos extras quando o link é clicado.
+document.getElementById("pulsating-linkk").addEventListener("click", function() {
+    // Adicione aqui qualquer ação que você deseja que ocorra quando o link é clicado.
+    alert("Você clicou no link pulsante!");
+  }); 
+  

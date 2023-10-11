797627857bc240aa020656650e3e77542e303022
Delfinzito <146035305+Delfinzito@users.noreply.github.com>
2023-10-11 00:07:54 -0300

    novo
    dasdad

--- js/dadsa.js
+++ js/dadsa.js
@@ -3,10 +3,3 @@ document.getElementById("pulsating-link").addEventListener("click", function() {
     // Adicione aqui qualquer ação que você deseja que ocorra quando o link é clicado.
     alert("Você clicou no link pulsante!");
   });
-
- // O JavaScript não é estritamente necessário para fazer o link pulsar, mas pode ser usado para adicionar comportamentos extras quando o link é clicado.
-document.getElementById("pulsating-linkk").addEventListener("click", function() {
-    // Adicione aqui qualquer ação que você deseja que ocorra quando o link é clicado.
-    alert("Você clicou no link pulsante!");
-  }); 
-  
